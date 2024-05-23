4f399c3f19096edb17daf50857b2daf0e42dcb4f
aleyrasg <126541501+aleyrasg@users.noreply.github.com>
2024-05-23 00:32:51 -0600

    prueba2

--- obj/Debug/net8.0/RelojArena.AssemblyInfo.cs
+++ obj/Debug/net8.0/RelojArena.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RelojArena")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe791de88505abc54f96c2b026f8318c7d2368e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RelojArena")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RelojArena")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
